63520ba6fcf7230b037c0bfe08b617654778c4d8
Robert Brem <robert.brem@adesso.ch>
2018-09-28 00:21:41 +0200

    [TEST] add sleep that test succeed

--- UIT/2_addNewUser.js
+++ UIT/2_addNewUser.js
@@ -7,17 +7,19 @@ module.exports = {
 
       .waitForElementVisible('div[class=col-md-offset-1] h1', 2000)
       .assert.containsText('div[class=col-md-offset-1] h1', 'Welcome to User App!')
-      .assert.visible('#addUserLink')
+
+      .waitForElementVisible('#addUserLink', 2000)
       .click('#addUserLink')
+
       .waitForElementVisible('#email', 2000)
       .setValue('#email', 'testUser@adesso.ch')
       .assert.visible('#firstName')
       .setValue('#firstName', 'F123456789')
       .assert.visible('#lastName')
       .setValue('#lastName', 'L123456789')
-      .assert.visible('button')
-
-      .click('button')
+      .assert.visible('.btn-success')
+      .click('.btn-success')
+      .pause(250)
 
       .end();
   },
